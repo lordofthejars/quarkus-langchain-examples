6c60b4d645f35bf38c7ce7344ee4cbcefaef3ad9
Alex Soto <asotobu@gmail.com>
2024-03-15 10:13:16 +0100

    Updates versions

--- email-a-poem/src/main/java/io/quarkiverse/langchain4j/sample/EmailService.java
+++ email-a-poem/src/main/java/io/quarkiverse/langchain4j/sample/EmailService.java
@@ -14,7 +14,7 @@ public class EmailService {
     @Inject
     Mailer mailer;
 
-    @Tool("send the given content by email")
+    @Tool("send given content by email")
     public void sendAnEmail(String content) {
         Log.info("Sending an email: " + content);
         mailer.send(Mail.withText("sendMeALetter@quarkus.io", "A poem for you", content).setFrom("origin@quarkus.io"));
